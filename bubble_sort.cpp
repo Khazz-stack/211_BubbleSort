a28c6a78e38be9a534cc3c42e77e5d7837defd2a
Khazz <thelenaid@gmail.com>
2026-03-03 11:08:30 +0700

    input dari pengguna

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -15,6 +15,18 @@ void input()
         if (n <= 20)
             break;
         else
-        {}    
+        {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }    
+    }
+    cout << endl;
+    cout << "==================" << endl;
+    cout << "Masukan Elemen Array" << endl;
+    cout << "==================" << endl;
+
+    for ( int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
     }
 }
